b04d38c0d7b6b096d2fe318080507ca9678f7596
redstone <xiwenge@come-future.com>
2023-05-10 14:30:59 +0800

    chore: 调整文件目录及结构

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,22 @@ import ButtonLogClass, {
   buttonLogUtils,
   getDoubleButtonConfigs,
   getMaterialConfigs,
-} from './log-button';
+} from './button-log';
 import * as styleValue from './style';
+import {
+  emoji
+} from './special-text';
 
 export {
-  styleValue,
   buttonLog,
   materialButtonLog,
+  
   buttonLogUtils,
   getDoubleButtonConfigs,
   getMaterialConfigs,
+
+  styleValue,
+  emoji,
 };
 
 export default ButtonLogClass;
