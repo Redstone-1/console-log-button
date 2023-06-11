fab09c9491e0cba0d87a28c4b2097ca32e77d579
redstone <xiwenge@come-future.com>
2023-06-11 20:59:52 +0800

    chore: eslintrc 支持 node 语法不报错, 删除 lib 包(因为我们只运行在浏览器环境), 调整相关工程配置

--- vite.config.ts
+++ vite.config.ts
@@ -13,13 +13,6 @@ export default defineConfig({
           preserveModules: true, // 该选项将使用原始模块名作为文件名
           dir: 'es', // 输出的目录
           preserveModulesRoot: 'src' // 确保输出的目录和输入时的一致
-        },
-        {
-          format: 'cjs', // 指定模块化类型
-          entryFileNames: '[name].js',
-          preserveModules: true,
-          dir: 'lib',
-          preserveModulesRoot: 'src'
         }
       ]
     },
