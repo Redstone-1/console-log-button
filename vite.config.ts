e14f24b965e70fa9f28a7264c4d69ce74fae394b
redstone <xiwenge@come-future.com>
2023-10-08 15:52:34 +0800

    feat: 删除原来的写法, 准备替换新写法

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,22 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
+    lib: { // https://cn.vitejs.dev/config/build-options.html#build-lib
+      entry: './index.ts', // 定义作为库的入口是哪个
+    },
     target: 'modules', // 详情参考官方文档：https://cn.vitejs.dev/config/build-options.html#build-target
     minify: true, // 是否开启代码压缩
     rollupOptions: {
       input: ['src/index.ts'], // 打包的入口文件 https://cn.rollupjs.org/configuration-options/#input
       output: [ // 产物输出配置
         {
-          format: 'es', // 指定模块化类型 https://cn.rollupjs.org/configuration-options/#output-format
+          format: 'umd', // 指定模块化类型 https://cn.rollupjs.org/configuration-options/#output-format
           entryFileNames: '[name].js', // 入口文件名，默认 https://cn.rollupjs.org/configuration-options/#output-entryfilenames
           preserveModules: true, // 该选项将使用原始模块名作为文件名
-          dir: 'es', // 输出的目录
+          dir: 'lib', // 输出的目录
           preserveModulesRoot: 'src' // 确保输出的目录和输入时的一致
         }
       ]
     },
-    lib: { // https://cn.vitejs.dev/config/build-options.html#build-lib
-      entry: './index.ts', // 定义作为库的入口是哪个
-    }
   },
 });
